942b6c1061404fb7bfe3c75129baf16e098b0859
Julian Battaglino <julianbattaglino@gmail.com>
2022-08-11 13:39:02 -0300

    Studio Ghibli Movies Api

---
+++ scripts.js
@@ -0,0 +1,55 @@
+//fetch data from movies using fetch ajax
+
+fetch('https://ghibliapi.herokuapp.com/films') //fetch data from movies using fetch ajax
+    .then(res => res.json()) //convert data to json
+    .then(data => { //display data in console
+        console.log(data);
+
+        //Loop foreach para recorrer el array de peliculas
+        data.forEach(pelicula => {
+
+            //crear un elemento para cada pelicula
+            const movieItem = document.createElement('div');
+            movieItem.classList.add('col-12', 'col-md-4', 'col-lg-3');
+            movieItem.innerHTML = `
+            <img class="movie-img" src="${pelicula.movie_banner}" alt="${pelicula.title}">
+            <h2 class="movie-title">${pelicula.title}</h2>
+            <h2 class="movie-title-romanised">${pelicula.original_title_romanised}</h2>
+            <p class="movie-extract">${pelicula.description}</p>
+            <h3 class="director-producer">Director: ${pelicula.director}</h3>
+            <h3 class="director-producer">Producer: ${pelicula.producer}</h3>
+
+
+            `;
+            //Agregar cards al nodo padre
+            document.querySelector('#movie-list').appendChild(movieItem);
+        }
+        );
+    }
+    );
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
